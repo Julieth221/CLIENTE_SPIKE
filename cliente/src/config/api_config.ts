89b1a0622441c850a18cbbe36fad73a84ebb9c72
Julieth221 <darlycuberos17@gmail.com>
2025-04-02 15:52:09 -0500

    Se creo el proyecto y los componentes basicos de SPIKE en angular

---
+++ cliente/src/config/api_config.ts
@@ -0,0 +1,11 @@
+export const API_URLS ={
+    MID:{
+        API_MID_SPIKE: 'localhost:8082/v1'
+    },
+    CRUD: {
+        API_CRUD_USUARIO: 'localhost:8088/v1',
+        API_CRUD_FINCA: 'localhost:8086/v1',
+        API_CRUD_CULTIVO: 'localhost:8085/v1',
+        API_CRUD_SENSORES: 'localhost:8087/v1'
+    }
+}
