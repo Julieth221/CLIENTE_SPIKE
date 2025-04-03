b5eca9cb4372185c9cb19b68d99e3f668f030503
Julieth221 <darlycuberos17@gmail.com>
2025-04-03 12:35:11 -0500

    Se creo el componente home

--- cliente/src/app/components/home/home.component.ts
+++ cliente/src/app/components/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  currentYear: number = new Date().getFullYear();
+
+  constructor(private router: Router) {}
+
+  goToLogin(){
+    this.router.navigate(['/login'])
+  }
 
 }
--- cliente/src/main.ts
+++ cliente/src/main.ts
@@ -1,6 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { LoginComponent } from './app/components/login/login.component';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './app/components/register/register.component';
 import { AppComponent } from './app/app.component';
+import { DashboardComponent } from './app/components/dashboard/dashboard.component';
+import { provideHttpClient } from '@angular/common/http';
+import { HomeComponent } from './app/components/home/home.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent,{
+    providers:[
+        provideRouter([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: 'dashboard', component: DashboardComponent}
+          ]),
+        provideAnimations(),
+        provideHttpClient()
+    ]
+}).catch(err => console.error(err));
