d13fe6f1764bd75c8607481d93c5707069fbbc5a
YAMITHSALC27 <salcedoyamith@gmail.com>
2025-04-03 18:08:45 -0500

    creacion_dashboard_sencillo

--- cliente/src/app/components/dashboard/dashboard.component.ts
+++ cliente/src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+    selector: 'app-dashboard',
+    providers: [provideNativeDateAdapter()],
+    imports: [
+        MatExpansionModule,
+        CommonModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatSidenavModule,
+        MatToolbarModule,
+    ],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
-
+    showFiller = false;
 }
