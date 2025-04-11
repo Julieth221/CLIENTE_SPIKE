6519e604126fa805616409db9e837755d481a59b
Julieth221 <darlycuberos17@gmail.com>
2025-04-11 16:50:35 -0500

    Envio de codigo de recuperación al correo exitoso y actualización de contraseña funcional

--- cliente/src/app/components/forgot-pass/forgot-pass.component.ts
+++ cliente/src/app/components/forgot-pass/forgot-pass.component.ts
@@ -8,6 +8,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatInputModule} from '@angular/material/input';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../../services/api.service';
+import { API_URLS } from '../../../config/api_config';
 
 @Component({
   selector: 'app-forgot-pass',
@@ -26,7 +28,7 @@ import { CommonModule } from '@angular/common';
 export class ForgotPassComponent { forgotPasswordForm: FormGroup;
   isLoading = false;
 
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private router: Router) {
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private router: Router, private apiService: ApiService) {
     this.forgotPasswordForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]]
     });
@@ -49,19 +51,38 @@ export class ForgotPassComponent { forgotPasswordForm: FormGroup;
 
   sendResetCode() {
     if (this.forgotPasswordForm.invalid) return;
-
+  
     this.isLoading = true;
-    setTimeout(() => {
-      this.isLoading = false;
-      this.snackBar.open('Se ha enviado el enlace de recuperación a su correo', 'Cerrar', {
-        duration: 5000,
-        panelClass: ['success-snackbar']
+  
+    const correo = this.forgotPasswordForm.value.email;
+  
+    const correoData = {
+      CorreoElectronico: correo
+    };
+  
+    this.apiService.put(`${API_URLS.MID.API_MID_SPIKE}/usuarios/${correo}`, correoData)
+      .subscribe({
+        next: (response: any) => {
+          console.log('Respuesta de forgot password', response);
+          this.snackBar.open('El código de recuperación fue enviado', 'Cerrar', {
+            duration: 4000
+          });
+           // Guardar el correo antes de ir a la siguiente vista
+          localStorage.setItem('recoveryEmail', correo);
+          this.isLoading = false;
+          this.router.navigate(['/verifyCode']);
+        },
+        error: (error) => {
+          console.error('Error al enviar el código');
+          this.isLoading = false;
+  
+          if (error.error && error.error.mensaje) {
+            this.snackBar.open('Error al enviar el código de recuperación', 'Cerrar', {
+              duration: 4000
+            });
+          }
+        }
       });
-
-    // Esperar a que el mensaje termine para navegar
-    setTimeout(() => {
-      this.router.navigate(['/verifyCode']); 
-    }, 3000); 
-  }, 2000);
   }
+  
 }
--- cliente/src/services/api.service.ts
+++ cliente/src/services/api.service.ts
@@ -13,10 +13,12 @@ export class ApiService{
     // Metodo get general
     get<T>(url:string): Observable<T>{
         return this.http.get<T>(url);
+        
     }
 
     post<T>(url:string, data:any): Observable<T>{
         return this.http.post<T>(url, data);
+        
     }
 
     put<T>(url:string, data:any): Observable<T>{
